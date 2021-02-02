e3eb692516b330e743401dc6fc883d4355afdf53
ivankaminskyi <ivan.kaminskyi@gmail.com>
2021-02-02 18:38:15 +0200

    fix request handler

--- src/functions/graphql/handlers/index.js
+++ src/functions/graphql/handlers/index.js
@@ -2,10 +2,8 @@ exports.processRequest = (controller) => (req, res, next) =>
   Promise.resolve()
     .then(() => controller(req, res))
     .then((result) =>
-      res.status(200).json(
-        JSON.stringify({
-          data: result,
-        })
-      )
+      res.status(200).json({
+        data: result,
+      })
     )
     .catch(next);
